97cd00f7cff2ed588ded11b32e72707cd1836a16
Joseph Parker <30281911+jparker0190@users.noreply.github.com>
2022-08-19 21:05:40 -0400

    adding blog feature

---
+++ add_comment.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <link rel="stylesheet"type="text/css"href="style.css">
+        <div id="nav">
+            <?php include 'include.htm';?>
+        </div>
+    </head>
+    <?php
+        //Get Heroku ClearDB connection information
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db = substr($cleardb_url["path"],1);
+        $active_group = 'default';
+        $query_builder = TRUE;
+        // Connect to DB
+        $DBConnect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+        //let me know if there isn't a db connection
+        if($DBConnect == false){
+            print "No Connection Made.";
+        }
+        else{
+            //variable for tablename
+            $Table = 'blog';
+
+            //variables to hold data from the form
+            $ticker = stripslashes($_POST['ticker']);
+            $comment = stripslashes($_POST['comment']);
+            $current_price = stripslashes($_POST['current_price']);
+            $ytdchange = stripslashes($_POST['ytdchange']);
+
+            $SQLInsert = "insert into $Table(ticker, comment, current_price, ytdchange) values ('$ticker',$comment,$current_price,$ytdchange)";
+
+            //tell me if the above query string doesn't work
+            if(mysqli_query($DBConnect,$SQLInsert)){
+                print"Added to DB";
+            }
+            else{
+                print "Something is wrong.";
+            }
+            mysqli_close($DBConnect);
+
+        }
+    ?>
+</html>
---
+++ blog.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+
+    <link rel="stylesheet"type="text/css"href="style.css">
+        <div id="nav">
+            <?php include 'include.htm';?>
+        </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-dark">
+        <div class="container-fluid">
+
+
+            <div id="cards">
+                    
+                </div>
+                <form method="post" action="add_stock_portfolio.php">
+                    <div class="mb-3">
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Ticker">
+                    </div>
+                    <div class="mb-3">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"placeholder="Enter Content"></textarea>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            
+
+
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+        </div>
+        </div><!--ending div for container-->
+
+    </body>
+</html>
