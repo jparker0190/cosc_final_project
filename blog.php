55bccf4b50c5dda577ef74dffd1f5545e239deaf
Joseph Parker <30281911+jparker0190@users.noreply.github.com>
2022-08-19 23:28:31 -0400

    adding li lists

--- blog.php
+++ blog.php
@@ -1,12 +1,10 @@
 <html>
     <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <link rel="stylesheet"type="text/css"href="style.css">
         <?php include 'include.htm';?>
     </head>
     <body>
-        <div class="container-fluid">
+        <div class="container">
             <div id="cards-title">
                 Please enter your comments on your stock below.
             </div>
@@ -47,17 +45,13 @@
 
                         if(mysqli_num_rows($Query)>0){
                             
-
+                            print"<ul>";
                             while ($Row = mysqli_fetch_assoc($Query)){
-                                print`<div class="card" style="width: 18rem;">
-                                <div class="card-body">
-                                  <h5 class="card-title">{$Row['ticker']} Price - {$Row['current_price']} YTD_Change - {$Row['ytdchange']}</h5>
-                                  <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                  <p class="card-text">{$Row['blogs']}</p>
-                                  <a href="#" class="card-link">Card link</a>
-                                  <a href="#" class="card-link">Another link</a>
-                                </div>
-                              </div>`;
+                                print`<li>
+                                    <p> Ticker: {$Row['ticker']} - Price: {$Row['current_price']} - YTDChange: {$Row['ytdchange']}</p>
+                                    <p>{$Row['blogs']}</p>
+                                    </li></ul>
+                                `;
                                 }
                         }
                         else{
